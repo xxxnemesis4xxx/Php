b121ff380f252de783c4d7a9998a07de41ccb954
xxxnemesis4xxx <magnitude9@live.ca>
2015-02-10 13:33:19 -0500

    Partie 3 - Fichier Html

--- Php/b.php
+++ Php/b.php
@@ -14,6 +14,32 @@
 		}
 		else echo "L'inscription est nécessaire!";
 
+
+		if (file_exists("Lab3phpFichier.txt"))
+		  	$fp = fopen("Lab3phpFichier.txt", "r");
+		else{
+		  	echo "Fichier introuvable !<br>Importation stoppée.";
+		  	exit();
+		}
 		?>
+		<?php
+		echo "<br/><br/>";
+		$compteur = 0;
+		while (!feof($fp)){ 
+		  	$ligne = fgets($fp,4096);
+		  	$liste = explode("|",$ligne);
+				if (strcasecmp($liste[1],$_COOKIE['acookie']) == 1 || $compteur != 0)
+				{
+		  		$nom = $liste[0];
+		  		$valeur = $liste[1];
+		  		echo "$nom : $valeur<br>";
+					if ($compteur == 7)
+						$compteur = 0;
+					else
+						++$compteur;		
+				}
+		}	
+		fclose($fp); 
+	?>
 	</body>
 </html>
