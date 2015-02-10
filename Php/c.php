91e82100606f2c28da53f3262acfbbf61e5a73e1
xxxnemesis4xxx <magnitude9@live.ca>
2015-02-10 13:43:14 -0500

    Partie 3 - Fichier php

---
+++ Php/c.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<html>
+	<?php
+			header('Content-Type: text/html; charset=utf-8');
+			setlocale(LC_ALL, 'fr_CA');
+	?>
+	<head>
+		<title>Laboratoire 4</title>	
+	</head>
+	<body>
+<?php
+$_SESSION['session'] = $_REQUEST['prenom'] . ", " .$_REQUEST['nom'];
+?>
+<p>
+Je vous remercie d'être venu sur notre site internet <?php echo $_REQUEST['prenom'] . " " .$_REQUEST['nom']; ?>
+</p>
+<a href="d.php">Page suivante</a>
+	</body>
+</html>
